b2bfd675d24fdfa7b32691add8e24f14a6634c57
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-29 00:34:32 +0900

    test trailing zero for whole number

---
+++ iOSDev/CalculatorTest.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var displayValue = 0.5
+
+var maybeInt = Int(displayValue)
+
+var displayString = String(displayValue)
+
+print(maybeInt)
