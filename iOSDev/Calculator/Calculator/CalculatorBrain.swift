246bfcaaeb3c5644c29cfeb870683171084b124a
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-08-28 22:44:27 +0900

    basic digit display

---
+++ iOSDev/Calculator/Calculator/CalculatorBrain.swift
@@ -0,0 +1,21 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Hoang Nguyen Thai on 8/28/16.
+//  Copyright © 2016 Hoang NT. All rights reserved.
+//
+
+import Foundation
+
+class CalculatorBrain {
+    func setOperand(operand: Double) { }
+    
+    func performOperation(symbol: String) { }
+    
+    var result: Double {
+        get {
+            return 0.0
+        }
+    }
+}
