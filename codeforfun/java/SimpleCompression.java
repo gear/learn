c6fcd78f707e3de35739d628463a4b0d8c0281fb
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-09-01 21:38:31 +0900

    inline solution

--- codeforfun/java/SimpleCompression.java
+++ codeforfun/java/SimpleCompression.java
@@ -2,6 +2,10 @@ class SimpleCompression {
   public static void main(String[] args) {
     assert args.length == 1 : "Only accept one string.";
     System.out.println(SimpleCompression.BufferCompress(args[0]));
+    System.out.println("Inline compression:");
+    char[] org = args[0].toCharArray();
+    System.out.println("New string length: " + SimpleCompression.InplaceCompress(org));
+    System.out.println(org);
   }
 
   public static String BufferCompress(String str) throws NullPointerException {
@@ -27,10 +31,28 @@ class SimpleCompression {
   }
 
   public static int InplaceCompress(char[] str) throws NullPointerException {
-    if (str.length < 2):
-      return;
-    char cur = 0;
-    char seeker = 0;
+    if (str.length < 2)
+      return str.length;
+    char cur = str[0];
+    int writer = 0;
     int count = 1;
+    for (int i = 1; i < str.length; ++i) {
+      if (str[i] != cur) {
+        if (count > 1) {
+          for (char c : ("" + count).toCharArray())
+            str[writer++] = c;
+          count = 1;
+        }
+        str[writer++] = cur;
+        cur = str[i];
+      } else {
+        ++count;
+      }
+    }
+    if (count > 1)
+      for (char c : ("" + count).toCharArray())
+        str[writer++] = c;
+    str[writer++] = cur;
+    return writer; // Number of char in new string
   }
 }
