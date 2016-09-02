2c86837d40a2ec1f99076b4349f4efc466523887
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-09-02 16:01:03 +0900

    matrix rotation

--- codeforfun/java/MatrixRotate.java
+++ codeforfun/java/MatrixRotate.java
@@ -5,28 +5,45 @@ public class MatrixRotate {
   public static void main(String[] args) throws IOException {
     assert args.length == 1;
     BufferedReader f = new BufferedReader(new FileReader(args[0]));
+    // Read first line for matrix size
+    StringTokenizer st = new StringTokenizer(f.readLine());
+    int mSize = Integer.parseInt(st.nextToken());
+    int[][] matrix = new int[mSize][mSize];
+    for (int i = 0; i < mSize; ++i) {
+      st = new StringTokenizer(f.readLine());
+      for (int j = 0; j < mSize; ++j) {
+        matrix[i][j] = Integer.parseInt(st.nextToken());
+      }
+    }
+    System.out.println("Original matrix:");
+    MatrixRotate.PrintMatrix(matrix);
+    System.out.println("Rotated matrix:");
+    MatrixRotate.InplaceRotate(matrix);
+    MatrixRotate.PrintMatrix(matrix);
   }
 
   public static void InplaceRotate(int[][] matrix) throws NullPointerException {
-    if (matrix.length < 1)
+    int n = matrix.length;
+    if (n < 1)
       return;
-    int halfNCol = (matrix[0].length / 2) + (matrix[0].length % 2);
-    int halfNRow = matrix.length / 2;
-    for (int i = 0; i < halfNRow; ++i) {
-      for (int j = 0; i < halfNCol; ++j) {
-        Swap90(matrix, i, j);
+    int nLayers = n / 2;
+    for (int i = 0; i < nLayers; ++i) {
+      for (int j = i; j < (n-1-i); ++j) {
+        int tmp = matrix[i][j];
+        matrix[i][j] = matrix[n-1-j][i];
+        matrix[n-1-j][i] = matrix[n-1-i][n-1-j];
+        matrix[n-1-i][n-1-j] = matrix[j][n-1-i];
+        matrix[j][n-1-i] = tmp;
       }
     }
   }
 
-  public static void Swap90(int[][] matrix, sourceIndexi, sourceIndexj) {
-    // Rotation matrix
-    final int a = 0;
-    final int b = 1;
-    final int c = -1;
-    final int d = 0;
-    // Targets
-    int temp = matrix[sourceIndexi][sourceIndexj];
-    matrix[sourceIndexi][sourceIndexj]
+  public static void PrintMatrix(int[][] matrix) throws NullPointerException {
+    for (int i = 0; i < matrix.length; ++i) {
+      for (int j = 0; j < matrix.length; ++j) {
+        System.out.print(matrix[i][j] + " ");
+      }
+      System.out.println("");
+    }
   }
 }
