a2abae53a62b57e4355e48d9785abd7abe8cd227
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-09-03 22:11:05 +0900

    first sol linked list partition

--- codeforfun/java/LinkedList.java
+++ codeforfun/java/LinkedList.java
@@ -1,4 +1,78 @@
 import java.util.*;
 import java.io.*;
 
+class LinkedList {
+  public Node head;
 
+  public static void main(String[] args) {
+    LinkedList list = new LinkedList();
+    list.head = new Node(0);
+    list.head.AppendToEnd(new Node(2));
+    list.head.AppendToEnd(new Node(8));
+    list.head.AppendToEnd(new Node(5));
+    list.head.AppendToEnd(new Node(6));
+    list.head.AppendToEnd(new Node(7));
+    list.head.AppendToEnd(new Node(4));
+    list.head.AppendToEnd(new Node(9));
+    list.head.AppendToEnd(new Node(3));
+    list.head.AppendToEnd(new Node(1));
+    list.head.PrintToEnd();
+    System.out.println("Partition:");
+    list.Partition(5);
+    list.head.PrintToEnd();
+  }
+
+  public void Partition(int val) {
+    Node seeker = this.head;
+    Node larger = null;
+    Node smaller = null;;
+    Node tmp;
+    while (seeker != null) {
+      if (seeker.key >= val) {
+        if (larger != null) {
+          larger.AppendToEnd(seeker);
+        } else {
+          larger = seeker;
+        }
+      } else {
+        if (smaller != null) {
+          smaller.AppendToEnd(seeker);
+        } else {
+          smaller = seeker;
+        }
+      }
+      tmp = seeker.next;
+      seeker.next = null;
+      seeker = tmp;
+    }
+    smaller.AppendToEnd(larger);
+    this.head = smaller;
+  }
+}
+
+class Node {
+  public Node next;
+  public int key;
+  public Node prev;
+
+  public Node(int key) {
+    this.key = key;
+  }
+
+  public void AppendToEnd(Node n) {
+    Node seeker = this;
+    while (seeker.next != null) {
+      seeker = seeker.next;
+    }
+    seeker.next = n; 
+  }
+
+  public void PrintToEnd() {
+    Node seeker = this;
+    while (seeker.next != null) {
+      System.out.print(seeker.key + " -> ");
+      seeker = seeker.next;
+    }
+    System.out.println(seeker.key);
+  }
+}
