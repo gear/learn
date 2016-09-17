10e0215256e9b6d5d4ff57784b1ff7e995b1eea9
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-09-17 20:29:19 +0900

    print length of lcs

--- codeforfun/java/DP_LCS.java
+++ codeforfun/java/DP_LCS.java
@@ -7,14 +7,31 @@ public class DP_LCS {
         assert (args.length == 2) : "Invalid input.";
         String str1 = args[0];
         String str2 = args[1];
-        int[][] la = new int[10][10];    
-        System.out.println("Hi");
+        System.out.println(lcs(str1,str2));
     }
 
-    public static int lcs(int i, int j, int[][] arr) {
+    public static int mem(int i, int j, int[][] arr) {
         if (i < 0 || j < 0)
             return 0;
         else
             return arr[i][j];
+    }
+
+    public static int max(int a, int b) {
+        return (a >= b) ? a : b;
+    }
+
+    public static int lcs(String a, String b) {
+        int[][] arr = new int[a.length()][b.length()];
+        for (int i = 0; i < a.length(); ++i) {//
+            for (int j = 0; j < b.length(); ++j) {//
+                if (a.charAt(i) == b.charAt(j)) {//
+                    arr[i][j] = mem(i-1,j-1,arr) + 1;
+                } else {//
+                    arr[i][j] = max(mem(i,j-1,arr),mem(i-1,j,arr)); 
+                }
+            }
+        }
+        return arr[a.length()-1][b.length()-1];
     }
 }
