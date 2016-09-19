40d55f9268222b70db4c551aab3d56706901e945
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-09-19 23:40:45 +0900

    init coin sum problem

---
+++ codeforfun/java/CoinSum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class CoinSum {
+  public static void main(String[] args) {
+    assert args.length == 1;
+    int s = Integer.parseInt(args[0])
+
+  }
+}
