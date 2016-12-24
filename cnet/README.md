00a68c52bd123ee8228663e9f9612f47bcdbbd79
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2016-12-25 00:31:15 +0900

    netx tut

---
+++ cnet/README.md
@@ -0,0 +1,5 @@
+# Compilation of network science courses
+
+## Barabasi Network Science Course
+
+## GTech Network Science Course
---
+++ cnet/networkx/walks.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+# python=3.5.2
+# desc="path generators for networkx graph"
+
+def random(graph, length, start=None):
+ 
