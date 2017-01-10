0fd6ed741e5a84cdc10b5293470544b727609827
gear <hoangnt@ai.cs.titech.ac.jp>
2017-01-10 20:41:18 +0900

    parser

--- udacity_softwaredev/json_parser.py
+++ udacity_softwaredev/json_parser.py
@@ -27,17 +27,98 @@ def grammar(description, whitespace=r'\s*'):
     G = {' ': whitespace}
     description = description.replace('\t', ' ')  # no tabs
     for line in split(description, '\n'):
-     
+        lhs, rhs = split(line, ' => ', 1)
+        alternatives = split(rhs, ' | ')
+        G[lhs] = tuple(map(split, alternatives))
+    return G
 
-json_grammar = r"""
-Object => [{] Members [}] | [{] [}]
-Members => Pair [,] Members | Pair
-Pair => String [:] Value
-Array => [[] Elements []] | [[] []]
-Elements => Value [,] Elements | Value
-Value => String | Number | Object | Array | True | False | Null
-String => ["] Chars ["] | ["] ["]
-Chars => Char Chars | Char
-Char => \w
-Number => -?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?
-"""
+def decorator(d):
+    """Make function d a decorator: d wraps a fucntion fn."""
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+def memo(f):
+    """Decorator that caches the return value for each call
+    to f(args). Then when called again with same args, we 
+    can just look it up."""
+    cache = {}
+    def _f(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = result = f(*args)
+            return result
+        except TypeError:  # Some element in args cannot be hashed
+            return f(args)
+    return _f
+
+def parse(start_symbol, text, grammar):
+    """Return a (tree, remainder) pair. If remainder is an
+    empty string, it parsed the whole string. Failure iff 
+    the remainder is None. This is a deterministic PEG parser,
+    so rule order (left-to-right) matters. Do `E => T op E | T`,
+    putting the longest parse first; don't do `E => T | T op E`.
+    Also, no left recursion is allowed: no `E => E op T`."""
+    # Tokenizer is the white space symbol and a tuple
+    tokenizer = grammar[' '] + '(%s)'
+    
+    def parse_sequence(sequence, text):
+        result = []
+        for atom in sequence:
+            tree, text = parse_atom(atom, text)
+            if text is None: return Fail
+            result.append(tree)
+        return result, text
+
+    @memo
+    def parse_atom(atom, text):
+        if atom in grammar:
+            for alternative in grammar[atom]:
+                tree, rem = parse_sequence(alternative, text)
+                if rem is not None: return [atom]+tree, rem
+            return Fail
+        else:
+            m = re.match(tokenizer % atom, text)
+            return Fail if (not m) else (m.group(1), text[m.end():])
+
+    return parse_atom(start_symbol, text)
+
+JSON = grammar("""
+object => [{] members [}] | [{] [}]
+members => pair [,] members | pair
+pair => string [:] value
+array => [[] Elements []] | [[] []]
+elements => Value [,] Elements | Value
+value => String | Number | Object | Array | True | False | Null
+string => ["] Chars ["] | ["] ["]
+chars => Char Chars | Char
+char => \w
+number => 
+""", whitespace='\s')
+
+Fail = (None, None)
+
+def test():
+    assert json_parse('["testing", 1, 2, 3]') == (                      
+                       ['value', ['array', '[', ['elements', ['value', 
+                       ['string', '"testing"']], ',', ['elements', ['value', ['number', 
+                       ['int', '1']]], ',', ['elements', ['value', ['number', 
+                       ['int', '2']]], ',', ['elements', ['value', ['number', 
+                       ['int', '3']]]]]]], ']']], '')
+    
+    assert json_parse('-123.456e+789') == (
+                       ['value', ['number', ['int', '-123'], ['frac', '.456'], ['exp', 'e+789']]], '')
+    
+    assert json_parse('{"age": 21, "state":"CO","occupation":"rides the rodeo"}') == (
+                      ['value', ['object', '{', ['members', ['pair', ['string', '"age"'], 
+                       ':', ['value', ['number', ['int', '21']]]], ',', ['members', 
+                      ['pair', ['string', '"state"'], ':', ['value', ['string', '"CO"']]], 
+                      ',', ['members', ['pair', ['string', '"occupation"'], ':', 
+                      ['value', ['string', '"rides the rodeo"']]]]]], '}']], '')
+    print('tests pass')
+    return 'test pass'
+
+if __name__ == '__main__':
+    test()
